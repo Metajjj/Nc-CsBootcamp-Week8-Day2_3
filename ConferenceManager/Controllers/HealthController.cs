4ccd93dfa3fce5699e2b2d5e0e679d58c217e5f8
Metajjj <joolsjannaway@gmail.com>
2025-01-07 10:38:10 +0000

    Task1 - add health check

---
+++ ConferenceManager/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Server is active");
+        }
+    }
+}
