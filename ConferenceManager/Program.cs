7054654a01603fdb2e86dfe40f37e5e2d5d61d31
Metajjj <joolsjannaway@gmail.com>
2025-01-07 11:15:09 +0000

    Task 3 - adding auth setup

--- ConferenceManager/Program.cs
+++ ConferenceManager/Program.cs
@@ -1,6 +1,11 @@
 
+using System.Text;
+
 using ConferenceManager.Data;
 using ConferenceManager.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using Microsoft.IdentityModel.Tokens;
 
 namespace ConferenceManager
 {
@@ -17,12 +22,37 @@ namespace ConferenceManager
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+                //Adding my custom controls
             builder.Services.AddScoped<IEventService, EventService>();
             builder.Services.AddScoped<IEventModel, EventModel>();
 
+            //Adding authentication
+            var key = Encoding.UTF8.GetBytes("Key-must-be-longer-than-32-chars-(256bytes)-else-debug-problems");
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = "your-name",
+                    ValidateAudience = true,
+                    ValidAudience = "your-app-name",
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
 
 
             var app = builder.Build();
+
+            app.UseAuthentication(); app.UseAuthorization();
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -32,8 +62,6 @@ namespace ConferenceManager
             }
 
             app.UseHttpsRedirection();
-
-            app.UseAuthorization();
 
             app.MapControllers();
 
